124a1f05099b7363c13a6103dbbf42332b1e9083
HectorBalderas <eduardo.balderas@cuadra.com.mX>
2025-10-13 12:41:29 -0600

    actualizacion

--- FrontEndCSC/src/components/CU/Perfil.jsx
+++ FrontEndCSC/src/components/CU/Perfil.jsx
@@ -5,7 +5,6 @@ function Perfil() {
     <div>
       <h1>Perfil Component</h1>
       <p>This is the Perfil component.</p>
-      <p>Additional information can go here.</p>
       <p>More details can be added as needed.</p>
     </div>
   );
