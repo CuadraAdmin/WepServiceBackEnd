5ffa65d2ad6692fa550652ec92136f8726a0b9b3
HectorBalderas <eduardo.balderas@cuadra.com.mX>
2025-10-13 12:35:58 -0600

    cambio prueba

--- FrontEndCSC/src/components/CU/Perfil.jsx
+++ FrontEndCSC/src/components/CU/Perfil.jsx
@@ -6,6 +6,7 @@ function Perfil() {
       <h1>Perfil Component</h1>
       <p>This is the Perfil component.</p>
       <p>Additional information can go here.</p>
+      <p>More details can be added as needed.</p>
     </div>
   );
 }
