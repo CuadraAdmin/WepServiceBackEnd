1c404abc8b9c50af76b0e863f31c17774a958b84
HectorBalderas <eduardo.balderas@cuadra.com.mX>
2025-10-29 13:48:05 -0600

    se cargan entidades nuevas

---
+++ BackEndCSC/WepServiceBackEnd/BE/CM/MarcasBE.cs
@@ -0,0 +1,42 @@
+﻿namespace WebServiceBackEnd.BE.CM
+{
+    public class MarcasBE
+    {
+        public int Marc_Id { get; set; }
+        public int Empr_Id { get; set; }
+        public string? Marc_Consecutivo { get; set; }
+        public string? Marc_Pais { get; set; }
+        public string? Marc_SolicitudNacional { get; set; }
+        public string? Marc_Registro { get; set; }
+        public string? Marc_Marca { get; set; }
+        public string? Marc_Diseno { get; set; }
+        public string? Marc_Clase { get; set; }
+        public string? Marc_Titular { get; set; }
+        public string? Marc_Figura { get; set; }
+        public string? Marc_Titulo { get; set; }
+        public string? Marc_Tipo { get; set; }
+        public string? Marc_Rama { get; set; }
+        public string? Marc_Autor { get; set; }
+        public string? Marc_Observaciones { get; set; }
+        public DateTime? Marc_FechaSolicitud { get; set; }
+        public DateTime? Marc_FechaRegistro { get; set; }
+        public string? Marc_Dure { get; set; }
+        public DateTime? Marc_Renovacion { get; set; }
+        public string? Marc_Oposicion { get; set; }
+        public string? Marc_ProximaTarea { get; set; }
+        public DateTime? Marc_FechaSeguimiento { get; set; }
+        public DateTime? Marc_FechaAviso { get; set; }
+        public bool Marc_Estatus { get; set; }
+        public string? Marc_CreadoPor { get; set; }
+        public DateTime? Marc_CreadoFecha { get; set; }
+        public string? Marc_ModificadoPor { get; set; }
+        public DateTime? Marc_ModificadoFecha { get; set; }
+
+
+        public EmpresaBE objEmpresaBE { get; set; }
+
+        // Propiedades auxiliares para operaciones
+        public int? Accion { get; set; }
+        public bool Marc_FiltroEstatus { get; set; }
+    }
+}
---
+++ BackEndCSC/WepServiceBackEnd/BE/EmpresaBE.cs
@@ -0,0 +1,22 @@
+﻿using WebServiceBackEnd.BE.CM;
+
+namespace WebServiceBackEnd.BE
+{
+    public class EmpresaBE
+    {
+        public int Empr_Id { get; set; }
+        public string? Empr_Clave { get; set; }
+        public string? Empr_Nombre { get; set; }
+        public string? Empr_PermisoGenerado { get; set; }
+        public bool Empr_Estatus { get; set; }
+        public string? Empr_CreadoPor { get; set; }
+        public DateTime? Empr_CreadoFecha { get; set; }
+        public string? Empr_ModificadoPor { get; set; }
+        public DateTime? Empr_ModificadoFecha { get; set; }
+
+        // Propiedades auxiliares para operaciones
+        public int? Accion { get; set; }
+        public bool Empr_FiltroEstatus { get; set; }
+
+    }
+}
