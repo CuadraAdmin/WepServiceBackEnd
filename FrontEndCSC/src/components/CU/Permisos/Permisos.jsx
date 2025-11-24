4feacf3bf59fe7889b91cd47db5e16099bdd56d1
HectorBalderas <eduardo.balderas@cuadra.com.mX>
2025-11-24 12:56:47 -0600

    acomodo de alerta de activar y descativar permiso

--- FrontEndCSC/src/components/CU/Permisos/Permisos.jsx
+++ FrontEndCSC/src/components/CU/Permisos/Permisos.jsx
@@ -848,7 +848,7 @@ function Permisos({ token, userData }) {
             <div className="p-8">
               <p className="text-center text-stone-600 mb-6 leading-relaxed">
                 ¿Estás seguro de que deseas desactivar el permiso{" "}
-                <span className="font-bold text-stone-900 block mt-2 text-lg">
+                <span className="font-bold text-stone-900 block mt-2 text-lg break-words">
                   "{permisoToDelete.Perm_Nombre}"
                 </span>
                 ?
@@ -906,7 +906,7 @@ function Permisos({ token, userData }) {
             <div className="p-8">
               <p className="text-center text-stone-600 mb-6 leading-relaxed">
                 ¿Estás seguro de que deseas activar el permiso{" "}
-                <span className="font-bold text-stone-900 block mt-2 text-lg">
+                <span className="font-bold text-stone-900 block mt-2 text-lg break-words">
                   "{permisoToActivate.Perm_Nombre}"
                 </span>
                 ?
